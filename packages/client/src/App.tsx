d834bf66fed9d5f8688fb269fd2acfb3d8173e9e
willemclarke <willem.2@hotmail.com>
2024-01-26 15:26:03 +1100

    feat: add barebones UI which sends deps to backend

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,77 @@
-import React from 'react'
+import React from 'react';
+import { z } from 'zod';
 
-function App() {
+const depSchema = z.record(z.string());
 
-  return (
-    <div>whats changed</div>
-  )
+const schema = z.object({
+  dependencies: depSchema,
+  devDependencies: depSchema,
+});
+
+function parseVersion(version: string) {
+  const chars = version.split('');
+
+  if (chars[0] === '^' || chars[0] === '~') {
+    return version.slice(1);
+  }
+
+  return version;
 }
 
-export default App
+export default function App() {
+  const [input, setInput] = React.useState();
+
+  const onChange = React.useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(JSON.parse(e.target.value));
+  }, []);
+
+  const onSubmit = React.useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if (!input) {
+        return;
+      }
+
+      const unknown = schema.safeParse(input);
+
+      if (!unknown.success) {
+        return;
+      }
+
+      const deps = Object.entries(unknown.data.dependencies).map(([name, version]) => {
+        return { name, version: parseVersion(version) };
+      });
+      const devDeps = Object.entries(unknown.data.devDependencies).map(([name, version]) => {
+        return { name, version: parseVersion(version) };
+      });
+
+      const dependencies = [...deps, ...devDeps];
+
+      fetch('http://localhost:8080/dependencies', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dependencies),
+      })
+        .then((res) => res.json())
+        .then((res) => console.log(res));
+    },
+    [input]
+  );
+
+  return (
+    <div className="App">
+      <h1>package json changeset</h1>
+      <form onSubmit={onSubmit}>
+        <textarea
+          style={{ width: '300px', height: '500px' }}
+          onChange={onChange}
+          placeholder="paste package json here"
+        />
+        <button type="submit">Submit for processing</button>
+      </form>
+    </div>
+  );
+}
--- packages/server/main.ts
+++ packages/server/main.ts
@@ -1,1 +1,30 @@
-console.log("Hello via Bun!");
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import { z } from 'zod';
+
+const dependencySchema = z.object({ name: z.string(), version: z.string() });
+
+type Dependency = z.infer<typeof dependencySchema>;
+
+const app = express();
+const port = process.env.PORT ?? 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/dependencies', (req, res) => {
+  const unknown = z.array(dependencySchema).safeParse(req.body);
+
+  if (!unknown.success) {
+    return res.json(400).json('Unable to parse provided body');
+  }
+
+  // TODO: send parsed data off to service
+  console.log(unknown.data);
+  res.status(200).json(unknown.data);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
