914dc658622ffc100ea28b68400d8b389e06d1c1
Ben heath <benjamin.d.heath@outlook.com>
2024-02-03 23:28:46 +1000

    chore: move gh client to seperate file

--- packages/server/src/utils.ts
+++ packages/server/src/utils.ts
@@ -1,7 +1,7 @@
 import semver from 'semver';
 import { z } from 'zod';
 import type { Dependency } from '../../common/src/types';
-import parseLinkHeader from 'parse-link-header';
+import { cockhubClient } from './github-client';
 
 type Repository = {
   owner: string;
@@ -61,68 +61,6 @@ export async function getRepositoryInfo(dependency: Dependency): Promise<Reposit
   const splitUrl = parsed.data.repository.url.split('/');
   return { owner: splitUrl[3], name: splitUrl[4].split('.')[0], version: dependency.version };
 }
-
-type HTTPMethod = 'GET' | 'POST';
-
-const GITHUB_BASE_URL = 'https://api.github.com';
-
-class GithubClient {
-  public get = this.createMethod('GET');
-  public post = this.createMethod('POST');
-
-  private createMethod(method: HTTPMethod) {
-    return async function <A = unknown>(url: string, options?: RequestInit) {
-      const res = await fetch(GITHUB_BASE_URL + url, {
-        method,
-        ...options,
-        headers: {
-          ...options?.headers,
-          Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
-        },
-      });
-
-      const data = (await res.json()) as A;
-
-      return { data, res };
-    };
-  }
-
-  public async paginate<A>(url: string, options?: { stopPredicate?: (data: A) => boolean }) {
-    const { stopPredicate } = options ?? {};
-    const allData = [] as A[];
-
-    const { data, res } = await this.get<A[]>(url);
-
-    allData.push(...data);
-
-    if (stopPredicate) {
-      const shouldStop = data.some(stopPredicate);
-
-      if (shouldStop) {
-        return allData;
-      }
-    }
-
-    const paginationLinks = parseLinkHeader(res.headers.get('link'));
-
-    if (!paginationLinks) {
-      return allData;
-    }
-
-    const { next } = paginationLinks;
-    if (next) {
-      const nextData = await this.paginate<A>(next.url.split(GITHUB_BASE_URL)[1], {
-        stopPredicate,
-      });
-
-      allData.push(...nextData);
-    }
-
-    return allData;
-  }
-}
-
-const cockhubClient = new GithubClient();
 
 export async function getReleaseNotes(repository: Repository): Promise<ReleaseNotes> {
   const data = await cockhubClient.paginate<ReleaseNote>(
